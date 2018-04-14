1312148130494db237169b6f1f2e7ee0c77c831b
Lizzie <lizzie.sorin@hotmail.fr>
2018-04-13 18:48:44 -0700

    add delete button and action for link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,14 @@
 class LinksController < ApplicationController
+
+	def show
+		@link = Link.find(params[:id])
+	end
+
+	def destroy
+		@link = Link.find(params[:id])
+		@list = List.find(params[:id])
+		@link.destroy
+		redirect_to list_path(@list) 
+	end
+
 end
