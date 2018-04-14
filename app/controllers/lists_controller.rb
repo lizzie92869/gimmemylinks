df9481354e256d2814fa64e72b5486fbcf4d1ce4
Lizzie <lizzie.sorin@hotmail.fr>
2018-04-14 07:26:08 -0700

    delete a list feature

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,7 @@ class ListsController < ApplicationController
 		# binding.pry
 		@list = List.create(list_name_params)
 		list_id = @list.id
+		#To have the new list appear in the user's list, I need to create a fake link to link the list and the user
 		@link = Link.create(user_id: params[:list][:user_id], list_id: list_id, name: "fake", url: "http://fake.com", priority: "fake")
 		# binding.pry
 		redirect_to list_path(@list)
@@ -16,6 +17,12 @@ class ListsController < ApplicationController
 		@new_list = List.create
 		@list = List.find(params[:id]) 
 		@links = @list.links
+	end
+
+	def destroy
+		@list = List.find(params[:id]) 
+		@list.destroy
+		redirect_to root_path
 	end
 
 	private
