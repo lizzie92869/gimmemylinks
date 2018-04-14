9fe25867c587fe7cfe1b8c996f2d371fbb11e644
Lizzie <lizzie.sorin@hotmail.fr>
2018-04-13 20:50:23 -0700

    create of the link with the right parameters

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -8,6 +8,12 @@ class LinksController < ApplicationController
 		@link = Link.new
 	end
 
+	def create
+		@list = List.find(params[:list_id])
+		@link = Link.create(link_params)
+		redirect_to list_path(@list)
+	end
+
 	def show
 		@link = Link.find(params[:id])
 	end
@@ -17,6 +23,12 @@ class LinksController < ApplicationController
 		@list = List.find(params[:id])
 		@link.destroy
 		redirect_to list_path(@list) 
+	end
+
+	private
+
+	def link_params
+		params.require(:link).permit(:name, :url, :priority, :user_id, :list_id)
 	end
 
 end
