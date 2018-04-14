060006d977048c581b4bd5aee347e310c92aebe0
Lizzie <lizzie.sorin@hotmail.fr>
2018-04-13 23:18:58 -0700

    form_for create a new list which is associated with a user through a fake link, change association link has_many :users

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,31 @@
 class ListsController < ApplicationController
 
+
+	def create
+		# binding.pry
+		@list = List.create(list_name_params)
+		list_id = @list.id
+		@link = Link.create(user_id: params[:list][:user_id], list_id: list_id, name: "fake", url: "http://fake.com", priority: "fake")
+		# binding.pry
+		redirect_to list_path(@list)
+	end
+
+
 	def show
-		@list = List.find(params[:id])
+		# binding.pry
+		@new_list = List.create
+		@list = List.find(params[:id]) 
 		@links = @list.links
+	end
+
+	private
+
+	def list_name_params
+		params.require(:list).permit(:name)
+	end
+
+	def list_params
+		params.require(:list).permit(:name, :user_id)
 	end
 
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
 	def home
-		
+		@new_list = List.create
 	end
 	
 end
